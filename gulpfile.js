b920ef626b80da35af373325ed08694c1407c081
ianca <iancalaurentino@gmai.com>
2025-09-12 17:23:20 -0300

    Apricando animação de estrelas

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const terser = require('gulp-terser'); // troquei uglify por terser
 
-// Compila SCSS
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(terser()) // agora funciona com JS moderno
+        .pipe(gulp.dest('./dist/js'));
+}
+
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/styles'));
 }
 
-// Observa mudanças
-function watchFiles() {
-    gulp.watch('./src/styles/*.scss', styles);
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
 
-// Exporta
-exports.styles = styles;
-exports.watch = watchFiles;
-exports.default = styles;
+exports.default = gulp.parallel(styles, images, scripts);
+
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+};
