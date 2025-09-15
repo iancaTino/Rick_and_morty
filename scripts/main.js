6840b4d37e8d76f0981d8def458fee46b449a984
ianca <iancalaurentino@gmai.com>
2025-09-15 18:45:44 -0300

    Finalizada a parte de tabs e personagens, adicionadas fotos

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
-// Salva a posição antes de recarregar
-window.addEventListener('beforeunload', () => {
-    sessionStorage.setItem('scrollPos', window.scrollY);
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+            escondeTodasAbas();
+            aba.classList.add('personagens__list--is-active');
+
+            removeBotaoAtivo();
+            event.target.classList.add('personagens__tabs__button--is-active');
+        });
+    });
 });
 
-// Restaura a posição ao carregar
-window.addEventListener('load', () => {
-    const scrollPos = sessionStorage.getItem('scrollPos');
-    if (scrollPos) {
-        window.scrollTo(0, scrollPos);
-    }
-});
+// Função para esconder todas as abas
+function escondeTodasAbas() {
+    const abas = document.querySelectorAll('[data-tab-id]');
+    abas.forEach(aba => aba.classList.remove('personagens__list--is-active'));
+}
+
+// Função para remover o estado ativo de todos os botões
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    buttons.forEach(button => button.classList.remove('personagens__tabs__button--is-active'));
+}
